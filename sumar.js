ca4d5fe2bd3f88114d8b3c7fbc06404a10e0c3ca
camila2108 <gutierrezcamilamilagros02@gmail.com>
2022-11-14 21:31:17 -0300

    nuevo repo

---
+++ sumar.js
@@ -0,0 +1,5 @@
+let sumar = (...numbers) => numbers.reduce((acum, num)=> acum + num)
+
+module.exports = sumar; 
+// me enlaza un repositorio local con origen remoto : git remote add origin https://github.com/gutierrezcamilamilagros02/pruebaGit.git. Si no hay error todo bien 
+// git remote -v  me hace una conexion entre la computadora y la nube . Fetch : es un gancho qu eme tra elas novedades del equipo. Push : me empuja el cambio 
